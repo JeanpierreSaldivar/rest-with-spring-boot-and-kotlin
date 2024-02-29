719c27c0c7f93c7c55a8dac374ea85b2e7d49216
JeanpierreSaldivarLeon <50430920+JeanpierreSaldivar@users.noreply.github.com>
2024-02-29 15:53:05 -0500

    First steps in Kotlin with Spring

---
+++ 05-FirstStepsWithKotlinwithSpringBoot/rest-with-spring-boot-and-kotlin-saldivar/src/main/kotlin/pe/com/saldivar/Greeting.kt
@@ -0,0 +1,3 @@
+package pe.com.saldivar
+
+class Greeting(val id: Long, val content: String)
---
+++ 05-FirstStepsWithKotlinwithSpringBoot/rest-with-spring-boot-and-kotlin-saldivar/src/main/kotlin/pe/com/saldivar/Startup.kt
@@ -0,0 +1,11 @@
+package pe.com.saldivar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
